17b596850eeea2ca4a7d9492aa3f02f145333f9c
youngyoom <tjdud9329@gmail.com>
2026-05-14 02:00:43 +0900

    [Feat] home 화면 구현 완료 및 이미지 추가

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,35 +2,77 @@ import MyIngredientCard from '../components/MyIngredientCard';
 import SeasonalCard from '../components/SeasonalCard';
 import RecipeItem from '../components/RecipeItem';
 import Navbar from '../components/Layouts/Navbar';
+import bell from '../assets/bell.png';
+import chevron from '../assets/chevron.png';
+
+const MOCK_DATA = {
+  Myingredients: [
+    { id: 1, name: '양배추', weight: '400g', day: '2' },
+    { id: 2, name: '치즈', weight: '50g', day: '8' },
+  ],
+  Seasonal: [
+    { id: 1, name: '매실', weight: '1kg', site: '사이트', price: '30000' },
+    { id: 2, name: '오이', weight: '1개', site: '사이트', price: '1000' },
+  ],
+  Recipes: [
+    { id: 1, title: '봄동 비빔밥', level: '초급', time: '10분' },
+    { id: 2, title: '냉이 된장국', level: '중급', time: '20분' },
+  ],
+};
 
 const Home = () => {
   return (
-    <div className="mx-[16px]">
-      <div className="flex justify-between max-w-[358px] h-[42px] mt-[63px]">
-        <h1>로고</h1>
-        <button>버튼</button>
+    <div className="relative mx-[16px]">
+      <div className="absolute flex justify-between w-full h-[42px] top-[63px]">
+        <h1 className="font-bold text-[28px]">로고</h1>
+        <button>
+          <img src={bell} alt="알림" className="w-[24px] h-[24px]" />
+        </button>
       </div>
-      <div className="max-w-[255px] h-[58px] mt-[115px]">
-        <p>반가워요! 지민님 🖐️</p>
-        <h3>오늘 봄 제철 봄동비빔밥 어때요?</h3>
+      <div className="absolute h-[58px] top-[115px]">
+        <p className="text-[16px]">반가워요! 지민님 🖐️</p>
+        <h3 className="font-bold text-[20px]">
+          오늘 봄 제철 봄동비빔밥 어때요?
+        </h3>
       </div>
-      <div className="flex items-center justify-center bg-[#F1FFE6] max-w-[121px] h-[30px] mt-[186px] border border-[#7FC24B] rounded-full px-[16px]">
+      <div className="absolute flex items-center justify-center bg-[#F1FFE6] max-w-[121px] h-[30px] top-[186px] border border-[#7FC24B] rounded-full px-[16px]">
         <p className="text-[#7FC24B] text-[10px] font-bold leading-none tracking-tight">
           2026년 5월 봄 시즌
         </p>
       </div>
-      <div className="flex max-w-[474px] h-[60px] mt-[248px]">
-        <MyIngredientCard />
+      <div className="absolute flex max-w-[474px] h-[60px] top-[248px] gap-[12px] item-center">
+        {MOCK_DATA.Myingredients.map((item, index) => (
+          <MyIngredientCard key={index} />
+        ))}
       </div>
-      <div className="flex flex-col max-w-[358px] h-[172px] mt-[172px]">
-        <h3>이번 달 제철 식재료</h3>
-        <SeasonalCard />
+
+      <div className="absolute  w-full  h-[172px] top-[328px]">
+        <div className="flex justify-between items-center w-full">
+          <h3 className="font-bold text-[18px]">이번 달 제철 식재료</h3>
+          <div>
+            <img src={chevron} alt="화살표" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+        <div className="mt-[12px] flex flex-col gap-[12px] ">
+          {MOCK_DATA.Seasonal.map((item, index) => (
+            <SeasonalCard key={index} />
+          ))}
+        </div>
       </div>
-      <div className="flex flex-col max-w-[358px] h-[508px] mt-[529pxpx]">
-        <h3>추천 레시피</h3>
-        <RecipeItem />
+
+      <div className="absolute w-full h-[508px] top-[529px]">
+        <div className="flex justify-between items-center w-full ">
+          <h3 className="font-bold text-[18px]">추천 레시피</h3>
+          <div>
+            <img src={chevron} alt="화살표" className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+        <div className="mt-[12px] flex gap-[12px]">
+          {MOCK_DATA.Recipes.map((item, index) => (
+            <RecipeItem key={index} />
+          ))}
+        </div>
       </div>
-      <Navbar />
     </div>
   );
 };
